aabb513195a8a1eff73fb94e91f7bc1031feba19
jacquelireg <35817940+jacquelireg@users.noreply.github.com>
2018-03-22 14:42:38 -0700

    save me

--- p5-blankExample/sketch.js
+++ p5-blankExample/sketch.js
@@ -1,70 +1,25 @@
-char currentkey= '1';
-PImage b;
+let monika;
+let x = -225;
+let speed = 5;
 
-float x;
-float y;
-float px;
-float py;
-float easing = 0.05;
 
-void setup(){
-  size(1000,1000);
-  background(255);
-  smooth();
-  frameRate(100);
-  strokeWeight(20);
+function preload() {
+  monika = loadImage('monika.jpg')
 }
 
-void draw() {
-  if(keyPressed) {
-    clear_print();
+function setup() {
+  createCanvas(500, 500)
 }
-  if(mousePressed) {
-   newkeyChoice();
+
+function draw() {
+  background(200);
+  image(monika, x, 10, monika.width / 2, monika.height / 2);
+  move();
+}
+
+function move() {
+  x = x + speed;
+  if (x >= width || x< -225) {
+    speed = -speed
   }
-  float targetX = mouseX;
-  x += (targetX - x) * easing;
-  float targetY = mouseY;
-  y += (targetY - y) * easing;
-  float weight = dist(x, y, px, py);
-  if(mousePressed)
-  line(x, y, px, py);
-  py = y;
-  px = x;
-  stroke(143,192,169);
-}
-
-void newkeyChoice() {
-  if (key == '1' ) {
-    //green
-    stroke(1434,192,169);
-  } else if (key == '2') {
-    //yellow
-    stroke(#FAF3DD);
-  } else if (key == '3') {
-    //lightgreen
-    stroke(#C8D5B9);
-  } else if (key == '4') {
-    //darkgreen
-    stroke(#74,124,89);
-  } else if (key == '5') {
-    //blue
-    stroke(#104,176,171);
-  } else if (key == '6') {
-    //erase
-    stroke(255);
-  } else if (key == '7') {
-    //just monika
-  b = loadImage("monika.jpg");
-  image(b, mouseX, mouseY);
-  strokeWeight(0);
-  }
-}
-
-void clear_print() {
-    if (key == 'x') {
-       background(255);
-    } else if (key == 'p') {
-       saveFrame("drawingprogram/drawing-####.png");
-    }
 }
